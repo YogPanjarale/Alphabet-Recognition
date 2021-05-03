363b8533f5d3d952fc1b7759f9d0fc83a41c26f8
YogPanjarale <yog.panjarale@gmail.com>
2021-05-03 20:38:40 +0530

    splitting data and scaling it

--- main.py
+++ main.py
@@ -19,3 +19,9 @@ Y = pd.read_csv("labels.csv")["labels"]
 print(pd.Series(Y).value_counts())
 classes = ['A', 'B', 'C', 'D', 'E','F', 'G', 'H', 'I', 'J', "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 nclasses = len(classes)
+
+#Splitting the data and scaling it
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state=9, train_size=3500, test_size=500)
+#scaling the features
+X_train_scaled = X_train/255.0
+X_test_scaled = X_test/255.0

82059562d383a4809f45467c722c6f5f09343d9d
YogPanjarale <yog.panjarale@gmail.com>
2021-05-03 20:34:09 +0530

    fetching stuff

--- main.py
+++ main.py
@@ -12,3 +12,10 @@ from sklearn.datasets import fetch_openml
 import os, ssl, time
 from PIL import Image
 import PIL.ImageOps
+
+#Fetching the data
+X = np.load('image.npz')['arr_0']
+Y = pd.read_csv("labels.csv")["labels"]
+print(pd.Series(Y).value_counts())
+classes = ['A', 'B', 'C', 'D', 'E','F', 'G', 'H', 'I', 'J', "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+nclasses = len(classes)
